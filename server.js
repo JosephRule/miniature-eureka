279e9764a3c05bee7803e4e9e26521c95a72b0f2
JosephRule <josephrule1@gmail.com>
2022-04-09 14:44:47 -0700

    fixing heroku

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
